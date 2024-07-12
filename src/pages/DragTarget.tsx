210967ee3b3fe96c3bf245b8f6163b464504fd86
HanseungYoo <trustyoo86@gmail.com>
2024-07-12 17:35:14 +0900

    feat: Add drag component

--- src/pages/DragTarget.tsx
+++ src/pages/DragTarget.tsx
@@ -1,37 +1,84 @@
-import { RefObject } from "react";
+import { MouseEventHandler, useReducer, useState } from "react";
 import Moveable, { OnDrag, OnResize } from 'react-moveable';
 
-type Props = {
-  ref: RefObject<HTMLDivElement>;
+// type Props = {
+//   ref: RefObject<HTMLDivElement>;
+// };
+
+type Position = {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+  transform?: string;
 };
 
-function DragTarget({ ref }: Props) {
-  const handleDrag = ({ left, top, transform }: OnDrag) => {
-    console.log(left, top);
-    // setPosition({ left, top, transform });
+type TargetProps = {
+  id: number;
+};
+
+function DragTarget({ id }: TargetProps) {
+  const [target, setTarget] = useState<EventTarget | null>(null);
+  const [elOptions, setElOptions] = useReducer(
+    (prev: Position, next: Partial<Position>) => ({
+      ...prev,
+      ...next,
+    }),
+    {
+      left: 0,
+      top: 0,
+      width: 200,
+      height: 50,
+    }
+  );
+
+  const handleMouseDown: MouseEventHandler = (e) => {
+    const nativeEvent = e.nativeEvent;
+
+    setTarget(() => nativeEvent.target);
   };
 
-  const handleResizing = ({ width, height, delta, clientX, clientY, direction, dist }: OnResize) => {
-    console.log('direction', direction, clientX, clientY, width, height, dist);
-    // delta[0] && setPosition({ width, top: clientX, left: clientY });
-    // delta[1] && setPosition({ height, top: clientX, left: clientY });
+  const handleDrag = ({ transform }: OnDrag) => {
+    // setElOptions({ left, top, transform });
+    setElOptions({ transform });
+  };
+
+  const handleResizing = ({ width, height, dist, transform }: OnResize) => {
+    console.log('direction', elOptions.left, elOptions.top, dist);
+    setElOptions({ width, height, transform });
   };
 
   return (
-    <Moveable
-      target={ref?.current}
-      container={null}
-      origin={true}
-      edge
-      resizable
-      throttleDrag={0}
-      draggable
-      throttleResize={0}
-      // scaleable
-      onDrag={handleDrag}
-      onResize={handleResizing}
-    // onScale={handleScale}
-    />
+    <>
+      <div
+        onMouseDown={handleMouseDown}
+        id={`moveable-${id}`}
+        key={`moveable-${id}`}
+        className={`target moveable-${id}`}
+        style={{
+          position: 'absolute',
+          width: elOptions.width,
+          height: elOptions.height,
+          left: elOptions.left,
+          top: elOptions.top,
+          transform: elOptions.transform,
+          border: '1px solid #bdbdbd',
+        }}>Moveable {id}</div>
+      <Moveable
+        target={target as HTMLElement}
+        container={null}
+        origin={true}
+        edge
+        resizable
+        throttleDrag={0}
+        draggable
+        throttleResize={0}
+        // scaleable
+        onDrag={handleDrag}
+        onResize={handleResizing}
+      // onScale={handleScale}
+      />
+    </>
   );
 }
 
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,90 +1,45 @@
 import EditorLayout from "@/components/Layout";
-import { useEffect, useReducer, useRef } from "react";
-import Moveable, { OnDrag, OnResize } from 'react-moveable';
-// import DragTarget from "./DragTarget";
+import { useReducer } from "react";
+import DragTarget from "./DragTarget";
 
-type Position = {
-  left: number;
-  top: number;
-  width: number;
-  height: number;
-  transform: string;
+// type Position = {
+//   left: number;
+//   top: number;
+//   width: number;
+//   height: number;
+//   transform?: string;
+// };
+
+type Element = {
+  id: number;
 };
 
 function EditorPage() {
-  const targetRef = useRef<HTMLDivElement>(null);
-  const [position, setPosition] = useReducer(
-    (prev: Position, next: Partial<Position>) => ({
+  const [element, setElement] = useReducer(
+    (prev: Element[], next: Element[]) => ([
       ...prev,
       ...next,
-    }),
-    {
-      left: 0,
-      top: 0,
-      width: 200,
-      height: 50,
-      transform: '',
-    }
+    ]),
+    []
   );
 
-  // useEffect(() => {
-  //   let moveable: Moveable | null = null;
-  //   console.log('moveable', moveable);
-  //   if (targetRef.current && !moveable) {
-  //     moveable = new Moveable(document.body, {
-  //       target: targetRef.current,
-  //       draggable: true,
-  //       resizable: true,
-  //     });
+  const handleAddElement = () => {
+    const len = element.length;
+    console.log('element', element);
+    setElement([{ id: len }]);
 
-  //     moveable.on('drag', ({ left, top }: OnDrag) => {
-  //       setPosition({ left, top });
-  //     });
-
-  //     moveable.on('resize', ({ delta, clientX, clientY, width, height, dist }: OnResize) => {
-  //       console.log(delta, clientX, clientY, width, height, dist);
-  //       delta[0] && setPosition({ width });
-  //       delta[1] && setPosition({ height });
-  //     });
-  //   }
-  // }, []);
-
-  //   setPosition({ transform });
-  // };
-
-  const handleDrag = ({ left, top, transform }: OnDrag) => {
-    console.log(left, top);
-    // setPosition({ left, top, transform });
-  };
-
-  const handleResizing = ({ width, height, delta, clientX, clientY, direction, dist }: OnResize) => {
-    console.log('direction', direction, clientX, clientY, width, height, dist);
-    // delta[0] && setPosition({ width, top: clientX, left: clientY });
-    // delta[1] && setPosition({ height, top: clientX, left: clientY });
   };
 
   return (
     <EditorLayout>
       <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-        <div className="target" style={{ position: 'absolute', top: position.top, left: position.left, border: '1px solid #bdbdbd', width: position.width, height: position.height }} ref={targetRef}>
-          Moveable!!
-        </div>
+        <button style={{ position: 'absolute', left: '90%', top: 0 }} onClick={handleAddElement}>
+          Moveable 추가
+        </button>
+        {
+          element.map((el) => <DragTarget id={el.id} key={`moveable-${el.id}`} />)
+        }
       </div>
-      <Moveable
-        ref={e => { this.moveable = e; }}
-        container={null}
-        origin={true}
-        edge
-        resizable
-        throttleDrag={0}
-        draggable
-        throttleResize={0}
-        // scaleable
-        onDrag={handleDrag}
-        onResize={handleResizing}
-      // onScale={handleScale}
-      />
-
     </EditorLayout>
   );
 }
